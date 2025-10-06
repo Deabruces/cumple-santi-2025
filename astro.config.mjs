5202eff4df61b09bd392e6b911efd1fad482f185
DeaBruces <deannybruces@gmail.com>
2025-10-05 23:16:18 -0300

    Add Vercel adapter to dependencies and remove unused API route for confirming attendance

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,20 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   // Configura tu URL de producción aquí para que los metadatos OG funcionen correctamente
   // Ejemplo: site: 'https://cumple-santiago.vercel.app'
   // site: 'https://tu-sitio.com',
-
+  site: 'https://cumple-santi-2025.deannybruces.com',
+  output: 'static',
   vite: {
     plugins: [tailwindcss()],
   },
-
   integrations: [react()],
+  adapter: vercel({
+    edgeMiddleware: true,
+  }),
 });
